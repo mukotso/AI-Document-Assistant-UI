ff8d9ce1a6a1c3eb3ef918ab4b5e29eeaed97882
Kelvin Mukotso <kelvinmukotso@gmail.com>
2024-07-10 09:35:35 +0300

    feat: login, register and dashboard UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, GlobalStyles } from "@mui/material";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Legend, Tooltip, BarElement } from "chart.js";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Legend, Tooltip, BarElement);
 
 function App() {
+  const globalStyles = {
+    a: {
+      color: "unset",
+      textDecoration: "none"
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Doc AI.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <GlobalStyles styles={globalStyles} />
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/components/common/Sidebar.js
@@ -0,0 +1,371 @@
+// import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import DashboardCustomizeOutlinedIcon from '@mui/icons-material/DashboardCustomizeOutlined';
+// import OtherHousesOutlinedIcon from '@mui/icons-material/OtherHousesOutlined';
+// import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+// import LogoutIcon from '@mui/icons-material/Logout';
+// import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Typography, colors } from '@mui/material';
+// import { images } from "../../assets";
+// import Animate from "./Animate";
+
+// const menus = [
+
+//   {
+//     title: "Overview",
+//     icon: <DashboardCustomizeOutlinedIcon />,
+//     state: "overview"
+//   },
+// ];
+
+// const serviceMenus = [
+//   {
+//     title: "Documents",
+//     icon: <OtherHousesOutlinedIcon />,
+//     state: "mortage"
+//   },
+//   {
+//     title: "Users",
+//     icon: <PeopleAltIcon />,
+//     state: "notification"
+//   },
+//   {
+//     title: "Logout",
+//     icon: <LogoutIcon />,
+//     state: "notification"
+//   }
+// ];
+
+
+
+// const Sidebar = ({ sidebarWidth }) => {
+//   // const container = window !== undefined ? () => window.document.body : undefined;
+
+//   const navigate = useNavigate();
+//   const activeState = "overview";
+
+//   const handleLogout = () => {
+//     // Perform any necessary logout logic here, such as clearing tokens
+
+//     // Redirect to home page
+//     navigate('/');
+//   };
+
+//   const MenuItem = (props) => {
+//     const { item, isActive } = props;
+
+//     const handleClick = () => {
+//       if (item.state === 'logout') {
+//         handleLogout();
+//       } else {
+//         // Handle other menu item clicks if necessary
+//       }
+//     };
+
+
+//   // const MenuItem = (props) => {
+//     return (
+//       <ListItem key={props.index} disableGutters disablePadding sx={{ py: 0.5 }}>
+//         <ListItemButton sx={{
+//           borderRadius: "10px",
+//           bgcolor: props.isActive ? colors.green[600] : "",
+//           color: props.isActive ? colors.common.white : "",
+//           "&:hover": {
+//             bgcolor: props.isActive ? colors.green[600] : "",
+//             color: props.isActive ? colors.common.white : "",
+//           }
+//         }}>
+//           <ListItemIcon sx={{
+//             minWidth: "40px",
+//             color: props.isActive ? colors.common.white : ""
+//           }}>
+//             {props.item.icon}
+//           </ListItemIcon>
+//           <ListItemText primary={
+//             <Typography fontWeight={600}>
+//               {props.item.title}
+//             </Typography>
+//           } />
+//         </ListItemButton>
+//       </ListItem>
+//     );
+//   };
+
+//   const drawer = (
+//     <Box
+//       padding={3}
+//       paddingBottom={0}
+//       display="flex"
+//       flexDirection="column"
+//       height="100vh"
+//       sx={{
+//         "::-webkit-scrollbar": {
+//           display: "none"
+//         }
+//       }}
+//     >
+//       {/* logo */}
+//       <Box sx={{ textAlign: "center", mb: 2 }}>
+//         <Animate type="fade" delay={1}>
+//           <img src={images.logo} alt="logo" height={60} />
+//         </Animate>
+//       </Box>
+//       {/* logo */}
+
+//       <Animate sx={{ flexGrow: 1 }}>
+//         <Paper
+//           elevation={0}
+//           square
+//           sx={{
+//             borderTopRightRadius: "10px",
+//             borderTopLeftRadius: "10px",
+//             p: 2,
+//             height: "100%",
+//             boxShadow: "rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px"
+//           }}
+//         >
+//           {/* menu group 1 */}
+//           <List>
+//             {menus.map((item, index) => (
+//               <MenuItem
+//                 key={index}
+//                 item={item}
+//                 isActive={item.state === activeState}
+//               />
+//             ))}
+//           </List>
+//           {/* menu group 1 */}
+
+//           {/* menu group 2 */}
+//           <List>
+//             <ListItem>
+//               <Typography fontWeight={600} mt={1} color={colors.grey[600]}>
+//                 Services
+//               </Typography>
+//             </ListItem>
+//             {serviceMenus.map((item, index) => (
+//               <MenuItem
+//                 key={index}
+//                 item={item}
+//                 isActive={item.state === activeState}
+//               />
+//             ))}
+//           </List>
+//           {/* menu group 2 */}
+//         </Paper>
+//       </Animate>
+//     </Box>
+//   );
+
+//   return (
+//     <Box
+//       component="nav"
+//       sx={{
+//         width: { md: sidebarWidth },
+//         flexShrink: { md: 0 }
+//       }}
+//     >
+//       {/* large screen */}
+//       <Drawer
+//         variant="permanent"
+//         sx={{
+//           display: { xs: "none", sm: "none", md: "block" },
+//           "& .MuiDrawer-paper": {
+//             boxSizing: "border-box",
+//             width: sidebarWidth,
+//             borderWidth: 0,
+//             bgcolor: "transparent",
+//             "::-webkit-scrollbar": {
+//               display: "none"
+//             }
+//           }
+//         }}
+//         open
+//       >
+//         {drawer}
+//       </Drawer>
+//       {/* large screen */}
+//     </Box>
+//   );
+// };
+
+// export default Sidebar;
+
+
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import DashboardCustomizeOutlinedIcon from '@mui/icons-material/DashboardCustomizeOutlined';
+import OtherHousesOutlinedIcon from '@mui/icons-material/OtherHousesOutlined';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Typography, colors } from '@mui/material';
+import { images } from "../../assets";
+import Animate from "./Animate";
+
+const menus = [
+  {
+    title: "Overview",
+    icon: <DashboardCustomizeOutlinedIcon />,
+    state: "overview"
+  },
+];
+
+const serviceMenus = [
+  {
+    title: "Documents",
+    icon: <OtherHousesOutlinedIcon />,
+    state: "mortage"
+  },
+  {
+    title: "Users",
+    icon: <PeopleAltIcon />,
+    state: "notification"
+  },
+  {
+    title: "Logout",
+    icon: <LogoutIcon />,
+    state: "logout"
+  }
+];
+
+const Sidebar = ({ sidebarWidth }) => {
+  const navigate = useNavigate();
+  const activeState = "overview";
+
+  const handleLogout = () => {
+    // Perform any necessary logout logic here, such as clearing tokens
+
+    // Redirect to home page
+    navigate('/');
+  };
+
+  const MenuItem = (props) => {
+    const { item, isActive } = props;
+
+    const handleClick = () => {
+      if (item.state === 'logout') {
+        handleLogout();
+      } else {
+        // Handle other menu item clicks if necessary
+      }
+    };
+
+    return (
+      <ListItem key={props.index} disableGutters disablePadding sx={{ py: 0.5 }}>
+        <ListItemButton
+          sx={{
+            borderRadius: "10px",
+            bgcolor: isActive ? colors.green[600] : "",
+            color: isActive ? colors.common.white : "",
+            "&:hover": {
+              bgcolor: isActive ? colors.green[600] : "",
+              color: isActive ? colors.common.white : "",
+            }
+          }}
+          onClick={handleClick}
+        >
+          <ListItemIcon
+            sx={{
+              minWidth: "40px",
+              color: isActive ? colors.common.white : ""
+            }}
+          >
+            {item.icon}
+          </ListItemIcon>
+          <ListItemText primary={<Typography fontWeight={600}>{item.title}</Typography>} />
+        </ListItemButton>
+      </ListItem>
+    );
+  };
+
+  const drawer = (
+    <Box
+      padding={3}
+      paddingBottom={0}
+      display="flex"
+      flexDirection="column"
+      height="100vh"
+      sx={{
+        "::-webkit-scrollbar": {
+          display: "none"
+        }
+      }}
+    >
+      {/* logo */}
+      <Box sx={{ textAlign: "center", mb: 2 }}>
+        <Animate type="fade" delay={1}>
+          <img src={images.logo} alt="logo" height={60} />
+        </Animate>
+      </Box>
+      {/* logo */}
+
+      <Animate sx={{ flexGrow: 1 }}>
+        <Paper
+          elevation={0}
+          square
+          sx={{
+            borderTopRightRadius: "10px",
+            borderTopLeftRadius: "10px",
+            p: 2,
+            height: "100%",
+            boxShadow: "rgba(145, 158, 171, 0.2) 0px 0px 2px 0px, rgba(145, 158, 171, 0.12) 0px 12px 24px -4px"
+          }}
+        >
+          {/* menu group 1 */}
+          <List>
+            {menus.map((item, index) => (
+              <MenuItem key={index} item={item} isActive={item.state === activeState} />
+            ))}
+          </List>
+          {/* menu group 1 */}
+
+          {/* menu group 2 */}
+          <List>
+            <ListItem>
+              <Typography fontWeight={600} mt={1} color={colors.grey[600]}>
+                Services
+              </Typography>
+            </ListItem>
+            {serviceMenus.map((item, index) => (
+              <MenuItem key={index} item={item} isActive={item.state === activeState} />
+            ))}
+          </List>
+          {/* menu group 2 */}
+        </Paper>
+      </Animate>
+    </Box>
+  );
+
+  return (
+    <Box
+      component="nav"
+      sx={{
+        width: { md: sidebarWidth },
+        flexShrink: { md: 0 }
+      }}
+    >
+      {/* large screen */}
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: "none", sm: "none", md: "block" },
+          "& .MuiDrawer-paper": {
+            boxSizing: "border-box",
+            width: sidebarWidth,
+            borderWidth: 0,
+            bgcolor: "transparent",
+            "::-webkit-scrollbar": {
+              display: "none"
+            }
+          }
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
+      {/* large screen */}
+    </Box>
+  );
+};
+
+export default Sidebar;
