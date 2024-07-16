8db8a904268439348bfcad8fbd3f2523e0d06a6d
kelvin Mukotso <kelvinmukotso@gmail.com>
2024-07-16 23:00:07 +0300

    feat:Dashboard API intergration, UI improvements and add content descriptions

--- src/pages/DocumentsPage.js
+++ src/pages/DocumentsPage.js
@@ -2,7 +2,7 @@ import { Grid, Box } from '@mui/material';
 import React from 'react';
 import Animate from '../components/common/Animate';
 import UploadDocument from '../components/document/UploadDocument';
-import DocumentStats from '../components/document/DocumentStatsSummary';
+
 
 const DocumentsPage = () => {
   return (
@@ -11,19 +11,13 @@ const DocumentsPage = () => {
         <Grid container spacing={3}>
           <Grid item xs={12}>
             <Box sx={{ marginTop: 18 }}> 
-              <Animate type="fade" delay={1.5} sx={{ height: "100%" }}>
+              <Animate type="fade" delay={1} sx={{ height: "100%" }}>
                 <UploadDocument />
               </Animate>
             </Box>
           </Grid>
 
-          <Grid item xs={12} container justifyContent="center">
-            <Animate delay={2.5}>
-              <Grid item xs={12}>
-                <DocumentStats />
-              </Grid>
-            </Animate>
-          </Grid>
+        
         </Grid>
       </Grid>
     </Grid>
